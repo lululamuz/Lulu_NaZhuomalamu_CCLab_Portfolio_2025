cf888d97a0ce001c5a38deff16ffbaba2fad762c
lululamuz <zhuo424@newschool.edu>
2025-12-05 13:24:30 -0500

    Add interactive face drawing with mouse control

--- project2.js
+++ project2.js
@@ -1,1 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(225, 255, 255);
+
+  let faceColor = map(mouseY, 0, height, 255, 150); // color changes with mouseY
+  fill(faceColor, 220, 200);
+
+  rectMode(CENTER); //face
+  noStroke();
+  rect(200, 200, 200, 200, 30); //x, y, width, height, corner radius
+
+  let eyeSize = map(mouseX, 0, width, 10, 40); // eye changes
+
+  fill(0); //eyes
+  ellipse(160, 170, eyeSize, eyeSize); //left eye
+  ellipse(240, 170, eyeSize, eyeSize); //right eye
+
+  noFill();  //smile mouth
+  stroke(0);
+  strokeWeight(4);
+  arc(200, 230, 80, 40, 0, PI); //x, y, width, height, start, stop
+}
+
 
