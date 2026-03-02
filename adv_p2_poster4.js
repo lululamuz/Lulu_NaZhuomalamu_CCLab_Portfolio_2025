d816217b4782cea98e391922660b5a0062eea194
lululamuz <zhuo424@newschool.edu>
2026-03-02 00:33:12 -0500

    Refactor poster code to use p5.js instance mode

--- adv_p2_poster4.js
+++ adv_p2_poster4.js
@@ -1,89 +1,91 @@
-let font;
-let word = "DANDELION";
-let fs = 110;
+function makePoster4(p) {
+  let font;
+  let word = "DANDELION";
+  let fs = 110;
 
-let seeds = [];
+  let seeds = [];
 
-function preload() {
-  font = loadFont("Fredoka-Regular.ttf");
-}
+  p.preload = () => {
+    font = p.loadFont("Fredoka-Regular.ttf"); // 路径要确认
+  };
 
-function setup() {
-  createCanvas(600, 600);
-  buildWord();
-}
+  p.setup = () => {
+    const c = p.createCanvas(600, 600);
+    c.parent("canvas-p4");
 
-function buildWord() {
-  seeds = [];
+    buildWord();
+  };
 
-  textFont(font);
-  textSize(fs);
+  function buildWord() {
+    seeds = [];
 
-  // 自动缩放避免溢出
-  while (textWidth(word) > width * 0.85) {
-    fs -= 5;
-    textSize(fs);
-  }
+    p.textFont(font);
+    p.textSize(fs);
 
-  let b = font.textBounds(word, 0, 0, fs);
-  let x = width / 2 - (b.x + b.w / 2);
-  let y = height / 2 - (b.y + b.h / 2);
-
-  let pts = font.textToPoints(word, x, y, fs, {
-    sampleFactor: 0.20
-  });
-
-  for (let p of pts) {
-    seeds.push(new Seed(p.x, p.y));
-  }
-}
-
-function draw() {
-background(255, 220, 90);
-
-  for (let s of seeds) {
-    s.update();
-    s.display();
-  }
-}
-
-class Seed {
-  constructor(x, y) {
-    this.home = createVector(x, y);
-    this.pos = createVector(x, y);
-    this.vel = createVector(0, 0);
-    this.alpha = 255;
-  }
-
-  update() {
-    // 鼠标像风一样推动种子
-    let d = dist(mouseX, mouseY, this.pos.x, this.pos.y);
-
-    if (d < 80) {
-      let wind = createVector(this.pos.x - mouseX, this.pos.y - mouseY);
-      wind.normalize();
-      wind.mult(0.8);
-      this.vel.add(wind);
+    while (p.textWidth(word) > p.width * 0.85) {
+      fs -= 5;
+      p.textSize(fs);
     }
 
-    // 慢慢减速
-    this.vel.mult(0.96);
-    this.pos.add(this.vel);
+    let b = font.textBounds(word, 0, 0, fs);
+    let x = p.width / 2 - (b.x + b.w / 2);
+    let y = p.height / 2 - (b.y + b.h / 2);
 
-    // 透明度随距离变化
-    let distFromHome = p5.Vector.dist(this.pos, this.home);
-    this.alpha = map(distFromHome, 0, 120, 255, 80);
+    let pts = font.textToPoints(word, x, y, fs, {
+      sampleFactor: 0.20
+    });
 
-    // 如果鼠标远离，慢慢回家
-    if (dist(mouseX, mouseY, this.pos.x, this.pos.y) > 120) {
-      this.pos.x = lerp(this.pos.x, this.home.x, 0.02);
-      this.pos.y = lerp(this.pos.y, this.home.y, 0.02);
+    for (let pt of pts) {
+      seeds.push(new Seed(pt.x, pt.y));
     }
   }
 
-  display() {
-    stroke(255, this.alpha);
-    strokeWeight(2);
-    point(this.pos.x, this.pos.y);
+  p.draw = () => {
+    p.background(255, 220, 90);
+
+    for (let s of seeds) {
+      s.update();
+      s.display();
+    }
+  };
+
+  class Seed {
+    constructor(x, y) {
+      this.home = p.createVector(x, y);
+      this.pos = p.createVector(x, y);
+      this.vel = p.createVector(0, 0);
+      this.alpha = 255;
+    }
+
+    update() {
+      let d = p.dist(p.mouseX, p.mouseY, this.pos.x, this.pos.y);
+
+      if (d < 80) {
+        let wind = p.createVector(this.pos.x - p.mouseX, this.pos.y - p.mouseY);
+        wind.normalize();
+        wind.mult(0.8);
+        this.vel.add(wind);
+      }
+
+      this.vel.mult(0.96);
+      this.pos.add(this.vel);
+
+      let distFromHome = p5.Vector.dist(this.pos, this.home);
+      this.alpha = p.map(distFromHome, 0, 120, 255, 80);
+
+      if (p.dist(p.mouseX, p.mouseY, this.pos.x, this.pos.y) > 120) {
+        this.pos.x = p.lerp(this.pos.x, this.home.x, 0.02);
+        this.pos.y = p.lerp(this.pos.y, this.home.y, 0.02);
+      }
+    }
+
+    display() {
+      p.stroke(255, this.alpha);
+      p.strokeWeight(2);
+      p.point(this.pos.x, this.pos.y);
+    }
   }
 }
+
+// ✅ 别忘了实例化
+new p5(makePoster4);
