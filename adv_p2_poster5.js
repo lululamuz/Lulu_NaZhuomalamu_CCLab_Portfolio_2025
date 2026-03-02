98a452e87221f082bf01315ecc122b7b479c7404
lululamuz <zhuo424@newschool.edu>
2026-03-02 00:33:39 -0500

    Refactor poster creation to use p5 instance methods

--- adv_p2_poster5.js
+++ adv_p2_poster5.js
@@ -1,84 +1,89 @@
-let font;
-let word = "DANDELION";
-let fs = 110;
+function makePoster5(p) {
+  let font;
+  let word = "DANDELION";
+  let fs = 110;
 
-let seeds = [];
+  let seeds = [];
 
-function preload() {
-  font = loadFont("Fredoka-Regular.ttf");
-}
+  p.preload = () => {
+    font = p.loadFont("Fredoka-Regular.ttf");
+  };
 
-function setup() {
-  createCanvas(600, 600);
-  buildWord();
-}
+  p.setup = () => {
+    const c = p.createCanvas(600, 600);
+    c.parent("canvas-p5");
 
-function buildWord() {
-  seeds = [];
+    buildWord();
+  };
 
-  textFont(font);
-  textSize(fs);
+  function buildWord() {
+    seeds = [];
 
-  while (textWidth(word) > width * 0.85) {
-    fs -= 5;
-    textSize(fs);
-  }
+    p.textFont(font);
+    p.textSize(fs);
 
-  let b = font.textBounds(word, 0, 0, fs);
-  let x = width / 2 - (b.x + b.w / 2);
-  let y = height / 2 - (b.y + b.h / 2);
-
-  let pts = font.textToPoints(word, x, y, fs, {
-    sampleFactor: 0.2
-  });
-
-  for (let p of pts) {
-    seeds.push(new Seed(p.x, p.y));
-  }
-}
-
-function draw() {
-  background(255, 210, 70); // 亮黄色
-
-  for (let s of seeds) {
-    s.update();
-    s.display();
-  }
-}
-
-class Seed {
-  constructor(x, y) {
-    this.home = createVector(x, y);
-    this.pos = createVector(x, y);
-    this.falling = false;
-    this.alpha = 255;
-  }
-
-  update() {
-    let d = dist(mouseX, mouseY, this.home.x, this.home.y);
-
-    // 鼠标靠近开始脱落
-    if (d < 60 && !this.falling) {
-      this.falling = true;
+    while (p.textWidth(word) > p.width * 0.85) {
+      fs -= 5;
+      p.textSize(fs);
     }
 
-    if (this.falling) {
-      this.pos.y += random(1, 3);
-      this.pos.x += random(-0.5, 0.5);
-      this.alpha -= 2;
+    let b = font.textBounds(word, 0, 0, fs);
+    let x = p.width / 2 - (b.x + b.w / 2);
+    let y = p.height / 2 - (b.y + b.h / 2);
 
-      // 掉到一定程度后回到原位
-      if (this.alpha <= 0) {
-        this.pos = this.home.copy();
-        this.alpha = 255;
-        this.falling = false;
+    let pts = font.textToPoints(word, x, y, fs, {
+      sampleFactor: 0.2
+    });
+
+    for (let pt of pts) {
+      seeds.push(new Seed(pt.x, pt.y));
+    }
+  }
+
+  p.draw = () => {
+    p.background(255, 210, 70);
+
+    for (let s of seeds) {
+      s.update();
+      s.display();
+    }
+  };
+
+  class Seed {
+    constructor(x, y) {
+      this.home = p.createVector(x, y);
+      this.pos = p.createVector(x, y);
+      this.falling = false;
+      this.alpha = 255;
+    }
+
+    update() {
+      let d = p.dist(p.mouseX, p.mouseY, this.home.x, this.home.y);
+
+      if (d < 60 && !this.falling) {
+        this.falling = true;
+      }
+
+      if (this.falling) {
+        this.pos.y += p.random(1, 3);
+        this.pos.x += p.random(-0.5, 0.5);
+        this.alpha -= 2;
+
+        if (this.alpha <= 0) {
+          this.pos = this.home.copy();
+          this.alpha = 255;
+          this.falling = false;
+        }
       }
     }
-  }
 
-  display() {
-    stroke(90, 60, 30, this.alpha);
-    strokeWeight(2);
-    point(this.pos.x, this.pos.y);
+    display() {
+      p.stroke(90, 60, 30, this.alpha);
+      p.strokeWeight(2);
+      p.point(this.pos.x, this.pos.y);
+    }
   }
 }
+
+// ✅ 实例化
+new p5(makePoster5);
